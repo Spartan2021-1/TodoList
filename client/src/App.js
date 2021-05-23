4786845af219e6c7a2f2b77348497a76aa7fb958
geun9716 <crintop@gmail.com>
2021-05-24 00:19:54 +0900

    make_todolist

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import logo from './assets/icons/logo.svg';
 import './App.css';
 import {Home, Login, Main} from './routes';
 
+
+
 function App() {
   return (
-    <div>
+    <>
       <Route exact path="/" component={Home}/>
       <Route path="/login" component={Login}/>
       <Route path="/main" component={Main}/>
-    </div>
+    </>
   );
 }
 
